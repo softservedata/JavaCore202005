db585325e7cb91209dead323d6cd52026b385133
Bohdan0708 <bohdansmail@gmail.com>
2020-05-24 14:04:00 +0300

    hw1

---
+++ src/com/softserve/edu/hw1/Appl.java
@@ -0,0 +1,22 @@
+package com.softserve.edu.hw1;
+
+
+		public class Appl {
+		    public static void main(String[] args) {
+
+		        int k = 16;
+		        int l = 50;
+		        System.out.println("k++= " + k++);
+		        System.out.println("++k= " + ++k);
+		        System.out.println("k+++k= ");
+		        System.out.println( k+++k);
+		        System.out.println("50---k= ");
+		        System.out.println( l---k);
+		        System.out.println("k*k= " + k*k);
+		        System.out.println("k/3= " +k/3);
+		        System.out.println("k%3= " +k%3);
+		        System.out.println("k++<++k= ");
+		        System.out.println(k++ < ++k);
+		    }
+		}
+
