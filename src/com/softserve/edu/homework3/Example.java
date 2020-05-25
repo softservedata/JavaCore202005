ff8387a5d6500f88b72860149671104efb808a34
YanaR <reshytko.jane@gmail.com>
2020-05-25 09:57:56 +0300

    added Example cLass with Idea

--- src/com/softserve/edu/homework3/Example.java
+++ src/com/softserve/edu/homework3/Example.java
@@ -1,4 +1,48 @@
 package com.softserve.edu.homework3;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Example {
+    import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+//import com.My;
+
+    /**
+     * @author JavaCore202005.
+     * <br>Version 0.1.
+     */
+    public class Appl {
+
+        /**
+         * Main <font color="red">Method</font>
+         * @param args form <b>CommnadLine</b>
+         * @throws <i>Exception</i>
+         */
+        public static void main(String[] args) throws Exception {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            //
+            System.out.print("Hello. What is your name? ");
+            String name = br.readLine();
+            //
+            System.out.print("How old are you? ");
+            int age = Integer.parseInt(br.readLine());
+            //
+            System.out.println("Hello " + name);
+            System.out.println("You are " + age);
+            br.close();
+		/*
+		byte b = 49;
+		System.out.println("b = " + b + "  b as Char = " + (char) b);
+		*/
+            System.out.print("0123456789");
+            Thread.sleep(4000);
+            //System.out.print("ab" + 13 + "cd");
+            System.out.print("ab" + ((char)13) + "cd");
+            //
+            //My m = new My();
+        }
+
+    }
 }
