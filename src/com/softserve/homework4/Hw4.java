f8ba52020fa372551f001419cde7fb8b2beeb315
Kate <katepadalka87@gmail.com>
2020-05-31 23:04:00 +0300

    add Hw4.java

--- src/com/softserve/homework4/Hw4.java
+++ src/com/softserve/homework4/Hw4.java
@@ -5,13 +5,18 @@ import java.awt.*;
 public class Hw4 {
 
     public class Employee {
-        public Employee(String name) {
-            this.name = name;
-        }
         private String name;
         private int rate;
         private double hours;
-        private static double totalSum;
+        static double totalSum;
+
+        public Employee(){
+        }
+
+        public Employee(String name, int rate) {
+            this.name = name;
+            this.rate = rate;
+        }
 
         public Employee(String name, int rate, double hours) {
             this.name = name;
@@ -19,7 +24,7 @@ public class Hw4 {
             this.hours = hours;
         }
 
-        public static double getTotalSum() {
+        static double getTotalSum() {
             return totalSum;
         }
     }
@@ -30,25 +35,19 @@ public class Hw4 {
         private double hours = 0.0;
         private static double totalSum = 0.0;
 
-        public double GetSalary() {
-            return rate * hours;
+        public double getSalary() {
+            return (rate * hours);
         }
 
         public double getBonuses() {
             return getSalary() * 0.1;//to calculate 10% from salary
         }
 
-        public void changeRate(int newRate) {
-            totalSum -= getSalary();
-            rate = newRate;
-            totalSum += getSalary();
+        public void changeRate (int rate){
+            this.rate = rate;
         }
 
         private double getSalary() {
-        }
-
-        public String toString() {
-            return "Employee: " + name + ", rate: " + rate + "hours: " + hours;
         }
 
         @Override
@@ -67,9 +66,9 @@ public class Hw4 {
             Employee e3 = new Employee ("Hanna", 3);
 
             System.out.println(//"Total sum - " + Employee.getTotalSum());
-            e1.changeRate(1);
-            e2.changeRate(1);
-            e3.changeRate(1);
+            e1.changeRate(2);
+            e2.changeRate(2);
+            e3.changeRate(2);
             System.out.println(//"Total sum - " + Employee.getTotalSum());
         }
 
