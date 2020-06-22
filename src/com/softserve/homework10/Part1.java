ff28101f218325c223001b80e4b9e42b3c01f923
igor5566 <volkoff5566@gmail.com>
2020-06-22 07:42:38 +0300

    Added HW10

--- src/com/softserve/homework10/Part1.java
+++ src/com/softserve/homework10/Part1.java
@@ -4,5 +4,25 @@ public class Part1 {
     public static void main(String[] args) {
         String json = "{\"id\":\"123\",\"name\":\"Ivan\",\"email\":\"ivan@gmail.com\",\"city\":\"Lviv\"}";
 
+        List<String> list = Arrays.asList(json.replaceAll("\"", ""));
+        String ss = list.get(0);
+        Map<String, String> map = new HashMap<>();
+        List<String> list1 = new ArrayList<>();
+
+        String[] arr = ss.split("[(,)]");
+        String[] arr2 = null;
+        for (String s : arr) {
+            arr2 = s.split("[(:)]");
+            for (String s1 : arr2) {
+                list1.add(s1);
+            }
+        }
+
+        for (int i = 0; i < list1.size(); i++) {
+            map.put(list1.get(i), list1.get(++i));
+        }
+        System.out.println(map);
     }
+
 }
+
