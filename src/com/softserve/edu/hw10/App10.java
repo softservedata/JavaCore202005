8bd0e0ca1d72169f280cfee84322ae104fb64b18
Bohdan0708 <bohdansmail@gmail.com>
2020-06-30 11:12:31 +0300

    homework 10

--- src/com/softserve/edu/hw10/App10.java
+++ src/com/softserve/edu/hw10/App10.java
@@ -1,24 +1,40 @@
 package com.softserve.edu.hw10;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class App10 {
-    public static void main (String [] args){
+    public static Map <String, String> parse(String json) {
+        Map<String, String> result = new HashMap<>();
+        String patternTemplate = "\\{\"id\":\"(\\d+)\",\"name\":\"(\\w+)\",\"email\":\"([\\w@.]+)\"(,\"city\":\"(\\w+)\")?}";
 
-           Scanner s = new Scanner(System.in);
-           String json = s.nextLine();
-           int index = json.indexOf("email");
-           System.out.println(json);
-           System.out.println(index);
-
-        Pattern p = Pattern.compile("email");
+        Pattern p = Pattern.compile(patternTemplate);
         Matcher m = p.matcher(json);
-        while (m.find()) {
-            System.out.print(json.substring(m.start(), m.end()));
+
+        if (m.matches()) {
+            result.put("id", m.group(1));
+            result.put("name", m.group(2));
+            result.put("email", m.group(3));
+            if (m.group(4) != null) {
+                result.put("city", m.group(5));
+            }
+        }
+        else {
+            throw new IllegalArgumentException("Input string is malformed!");
         }
 
+        return result;
+    }
 
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String json = scanner.nextLine();
+
+        System.out.println(json);
+        System.out.println(parse(json));
     }
 }
