00daeb71ffdc97c5c075c33e52af3943a3f42ff1
MaksymQC <maksym.serba@gmail.com>
2020-12-14 09:53:19 +0200

    add test_auto

--- src/test/java/unvalidLogin/UnvalidLogin.java
+++ src/test/java/unvalidLogin/UnvalidLogin.java
@@ -1,16 +1,17 @@
-package login;
+package unvalidLogin;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 import java.io.File;
 import java.util.concurrent.TimeUnit;
 
-public class Login {
+public class UnvalidLogin {
     @Test
-    public void validLogin() {
+    public void UnvalidLogin() {
         File file = new File("./src/drivers/chromedriver.exe");
         System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
 
@@ -18,15 +19,16 @@ public class Login {
 
         webDriver.manage().window().fullscreen();
         webDriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-
         webDriver.get("http://b2b-pds.stage.k8s.mc.gcf/login");
-
         System.out.println("Site was opened");
 
-        webDriver.findElement(By.xpath("//*[@placeholder='+38(050) XXX XX XX']")).sendKeys("+380635929001");
+        webDriver.findElement(By.xpath("//*[@placeholder='+38(050) XXX XX XX']")).sendKeys("+380630000000");
         webDriver.findElement(By.xpath("//*/button[2]")).click();
 
         Assert.assertTrue("Login isn ot valid", false);
         webDriver.quit();
+
+
     }
+
 }
